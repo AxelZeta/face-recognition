4a218a1b284319a90535dd2a8d18953b01a32e2c
AxelZeta <axelzanella93@gmail.com>
2020-06-14 14:11:56 +0200

    Updates

---
+++ precache-manifest.d5ebfe6b4256c5def2b652577c1420f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "969da6d210ac0f4571f68b257879cf46",
+    "url": "/face-recognition/index.html"
+  },
+  {
+    "revision": "c9ca95c291ed42bebc81",
+    "url": "/face-recognition/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "07e2a9ba4abddca83ddc",
+    "url": "/face-recognition/static/css/main.82ef622f.chunk.css"
+  },
+  {
+    "revision": "c9ca95c291ed42bebc81",
+    "url": "/face-recognition/static/js/2.34a9dd06.chunk.js"
+  },
+  {
+    "revision": "07e2a9ba4abddca83ddc",
+    "url": "/face-recognition/static/js/main.fc490623.chunk.js"
+  },
+  {
+    "revision": "2873375fc50fb304d268",
+    "url": "/face-recognition/static/js/runtime~main.4924a73e.js"
+  }
+]);
